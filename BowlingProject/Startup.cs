c9901682ea5807bf93e2ce5b096d35a6085a376e
Tony Au <tonyauu@uw.edu>
2017-02-16 13:06:19 -0800

    First Commit

---
+++ BowlingProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BowlingProject.Startup))]
+namespace BowlingProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
